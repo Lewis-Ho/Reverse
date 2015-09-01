b29175f0353d843636ee459899757d59cc92c649
Lewis-Ho <yiucheungho@Yius-MacBook-Pro.local>
2015-08-31 22:38:04 -0400

    init

---
+++ Reverse/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Reverse
+//
+//  Created by Yiu Cheung Ho on 8/27/15.
+//  Copyright (c) 2015 Lewis Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+    }
+
+}
+
