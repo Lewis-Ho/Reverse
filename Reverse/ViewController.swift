8c687aa0865ec620e254e527d810b65b28e4d6af
Lewis-Ho <yiucheungho@Yius-MacBook-Pro.local>
2015-09-02 23:48:40 -0400

    notes

--- Reverse/ViewController.swift
+++ Reverse/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // ViewDidLoad -- initial setup
+    // ViewWillAppear -- view before using - hiding/show button
+    // ViewDidAppear -- view after using - animation
+    // 
 
+    @IBOutlet weak var stopRecording: UIButton!
     @IBOutlet weak var recordingInProgress: UILabel!
     
     override func viewDidLoad() {
@@ -21,16 +26,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Hide stop button
+        stopRecording.hidden = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
         // Show recording text
         recordingInProgress.hidden = false
+        // Show stop recording button
+        stopRecording.hidden = false
         // Record audio
     }
 
     @IBAction func stopRecording(sender: AnyObject) {
         // Hide recording text
         recordingInProgress.hidden = true
+        
     }
 }
 
